eb2bf9a3b5dd92fca8378027d1b3650b1969e651
mahmoud <m410165835@gmail.com>
2023-12-30 11:51:51 +0200

    model was added

---
+++ model.py
@@ -0,0 +1,98 @@
+from collections import deque
+parent=[[0]*12 for i in range(12)]
+
+
+def GetNeighbours(x,y):
+    neighbours=[]
+    if x-1>=0: # top
+        neighbours.append((x-1,y))
+    
+    if x+1<10: # bottom
+        neighbours.append((x+1,y))
+    
+    if y+1<10: # right
+        neighbours.append((x,y+1))
+    
+    if y-1>=0: # left
+        neighbours.append((x,y-1))
+    
+    return neighbours
+
+def GetHuristic(x,y,Goals):
+    Min=10**10
+    for i,j in Goals:
+        N=abs(i-x)+abs(j-y)
+        if Min>N:
+            Min=N
+    return Min
+
+
+def Path(x,y,parent):
+    path=[(x,y)]
+    while parent[x][y]:
+        x,y=parent[x][y]
+        path.append((x,y))
+    return path[::-1]
+
+def GetStat(Id):
+    Stat=[]
+    F=open(f'stat/{Id}.txt')
+    for i in range(10):
+        B=F.readline().split(' ')
+        B[9]=B[9][0]
+        Stat.append(B)
+    F.close()
+    return Stat
+
+def GetBoard(Id):
+
+    monster=[]
+    Board= [[0]*10 for i in range(10)]
+    StartPoint=[0,0]
+    Goals=[]
+    stat=GetStat(Id)
+    for i in range(10):
+        for j in range(10):
+            match stat[i][j]:
+                case '#':
+                    Board[i][j]=-1
+
+                case 'P':
+                    Board[i][j]=1000
+                    StartPoint[0],StartPoint[1]=i,j
+
+                case 'E':
+                    Board[i][j]=1000
+                    Goals.append((i,j))
+
+                case 'M':
+                    Board[i][j]=0
+                    monster.append((i,j))
+
+                case _:
+                    Board[i][j]=1000
+                
+                
+                
+
+    
+    while len(monster):
+        x,y=monster.pop(0)
+        neighbours=GetNeighbours(x,y)
+        for i,j in neighbours:
+            if Board[x][y]+1<Board[i][j]:
+                Board[i][j]=Board[x][y]+1
+                monster.append((i,j))
+    return[Board,StartPoint,Goals]
+def solve(Id,algorithm=1):
+    Board,StartPoint,Goals=GetBoard(Id)
+    if algorithm ==1:
+        return BFS(StartPoint[0],StartPoint[1],Board,Goals)
+    elif algorithm ==2:
+        return DFS(StartPoint[0],StartPoint[1],Board,Goals)
+    elif algorithm ==3:
+        return UCS(StartPoint[0],StartPoint[1],Board,Goals)
+    elif algorithm==4:
+        return A_Star(StartPoint[0],StartPoint[1],Board,Goals)
+    else:
+        return greed(StartPoint[0],StartPoint[1],Board,Goals)
